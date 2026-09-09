4e01f311fb38cb9d8b17a63c5d81153e99ee2579
HelloWorldVictor <v.hamzat@alustudent.com>
2026-09-09 15:06:19 +0200

    Updates

---
+++ precache-manifest.1cc1caee1a01144e84a88ca2512aa3f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af61ef9cd572be75cb719d866796ec8",
+    "url": "/alu-web_react/index.html"
+  },
+  {
+    "revision": "3156ce6a624ea37c9361",
+    "url": "/alu-web_react/static/css/main.84cf323c.chunk.css"
+  },
+  {
+    "revision": "96cf15b92377d30e61e2",
+    "url": "/alu-web_react/static/js/2.07dcbf79.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/alu-web_react/static/js/2.07dcbf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3156ce6a624ea37c9361",
+    "url": "/alu-web_react/static/js/main.811efb7c.chunk.js"
+  },
+  {
+    "revision": "6f2f31cd926cf2e77253",
+    "url": "/alu-web_react/static/js/runtime-main.a2df1a0a.js"
+  }
+]);
